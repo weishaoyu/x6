754ce4806e2c68192dc241bdd90c87424f9f1a5e
pomelo-nwu <czynwu@outlook.com>
2019-12-29 17:04:19 +0800

    feat: add demo for ERGraph

--- packages/x6-er/examples/src/pages/index.tsx
+++ packages/x6-er/examples/src/pages/index.tsx
@@ -1,18 +1,49 @@
 import React from 'react';
 import styles from './index.css';
+import ERGraph from '../../../dist/index';
 
+const data = {
+  nodes: [
+    {
+      id: 'node-0',
+      x: 40,
+      y: 40,
+      width: 300,
+      height: 200,
+      render: () => {
+        return <div>hello node-0</div>;
+      },
+    },
+    {
+      id: 'node-1',
+      x: 140,
+      y: 40,
+      width: 300,
+      height: 200,
+      render: () => {
+        return (
+          <div
+            onClick={() => {
+              console.log('node-1');
+            }}
+          >
+            hello node-1
+          </div>
+        );
+      },
+    },
+  ],
+  edges: [
+    {
+      source: 'node-0',
+      target: 'node-1',
+    },
+  ],
+};
 export default function() {
   return (
     <div className={styles.normal}>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>
-          To get started, edit <code>src/pages/index.js</code> and save to reload.
-        </li>
-        <li>
-          <a href="https://umijs.org/guide/getting-started.html">Getting Started</a>
-        </li>
-      </ul>
+      <ERGraph data={data} />
     </div>
   );
 }
---
+++ packages/x6-er/src/components/Node.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Node = () => {
+  return <div>hello Node</div>
+}
+
+export default Node
